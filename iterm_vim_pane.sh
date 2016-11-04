7ff249a67a1f564f1fbae662e2c7a50f3034a997
Wilson Lam <wilson.j.lam@gmail.com>
2016-11-03 20:47:07 -0700

    iterm vim script

---
+++ iterm_vim_pane.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "$1" | osascript 3<&0 <<EOF
+	tell application "iTerm"
+		tell current session of current window
+			select(split vertically with default profile command "/usr/local/bin/vim $1") 
+		end tell
+	end tell
+EOF
