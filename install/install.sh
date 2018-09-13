4857f280e0bef110c8b5ec58c1bdcc8577a28092
Wilson Lam <wilsonl@qualtrics.com>
2018-09-12 20:51:53 -0700

    silence the sourcing of bash

--- install/install.sh
+++ install/install.sh
@@ -9,7 +9,13 @@ cat brew/cask_list | xargs brew cask install
 #dot files
 ./links.sh
 
+set +v
+source ~/.bash_profile
+set -v
+
 #vim
 vim -c PluginInstall -c qa
-cd ~/.vim/bundle/YouCompleteMe 
+cd ~/.vim/bundle/YouCompleteMe
 ./install.py
+
+npm install -g avn
