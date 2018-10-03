bac2642978c3bd2fd68cf2e9652b4b8a4e20dc07
Wilson Lam <wilson.j.lam@gmail.com>
2018-10-03 10:55:23 -0700

    update the installation script with alacritty install

--- install/install.sh
+++ install/install.sh
@@ -5,17 +5,17 @@ set -euv
 install_dir="$(dirname $BASH_SOURCE)"
 
 #directories
-if [ ! -d "~/workspace" ]; then
+if [ ! -d "$HOME/workspace" ]; then
 	mkdir ~/workspace
 fi
 
-if [ ! -d "~/personal" ]; then
+if [ ! -d "$HOME/personal" ]; then
 	mkdir ~/personal
 fi
 
 #brew installations
-cat "$install_dir/brew/list" | xargs brew install
-cat "$install_dirbrew/cask_list" | xargs brew cask install
+brew install < "$install_dir/brew/list"
+brew cask install < "$install_dir/brew/cask_list"
 
 #nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
@@ -24,9 +24,12 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 ./"$install_dir"/links.sh
 
 set +v
-source ~/.bash_profile
+source "$HOME/.bash_profile"
 set -v
 
 #vim
 vim -c PluginInstall -c qa
 ~/.vim/bundle/YouCompleteMe/install.py
+
+#alacritty
+./"$install_dir"/install_alacritty.sh
